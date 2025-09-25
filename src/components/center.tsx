c026af3a5dcf50c11952d09da35199f27e8badc7
fayzillo95 <ovovovlululutvata@gmail.com>
2025-09-26 01:15:07 +0500

    menu and sideBar icon

--- src/components/center.tsx
+++ src/components/center.tsx
@@ -7,38 +7,37 @@ import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon"
 import MicIcon from "@mui/icons-material/Mic"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import React from "react"
+import { Person } from "@mui/icons-material"
+import ViewSidebarIcon from '@mui/icons-material/ViewSidebar';
 
 function Center({props} : { props : {setOpen : Function}}) {
 
   return (
     <div className="flex flex-col justify-between w-full h-screen box-border">
-      {/* Header */}
+
       <Box className="p-3 border-b flex justify-between box-border">
-        <h1 className="text-2xl font-semibold">Muhammafrizo</h1>
-        <div className="flex">
-          <Button onClick={() => props.setOpen()}>open</Button>
+        <div className="flex gap-4 items-end">
+          <Person></Person>
+          <h1 className="text-2xl font-semibold">Muhammadrizo</h1>
+        </div>
+        <div className="flex items-center">
+          <Button onClick={() => props.setOpen()} ><ViewSidebarIcon color="disabled"></ViewSidebarIcon></Button>
           <MoreVertIcon></MoreVertIcon>
         </div>
       </Box>
 
-      {/* Messages area */}
       <Box className="flex-1 bg-amber-300 overflow-y-auto p-2">
-        {/* Bu yerda chat xabarlari chiqadi */}
       </Box>
 
-      {/* Input area */}
       <div className="flex w-full h-16 items-center gap-2 border-t p-2 box-border">
-        {/* File attach */}
         <IconButton color="default">
           <AttachFileIcon />
         </IconButton>
 
-        {/* Emoji/Stickers */}
         <IconButton color="default">
           <InsertEmoticonIcon />
         </IconButton>
 
-        {/* Input */}
         <TextField
           fullWidth
           placeholder="Xabar yozing..."
@@ -46,12 +45,10 @@ function Center({props} : { props : {setOpen : Function}}) {
           size="small"
         />
 
-        {/* Send button */}
         <Button variant="contained" color="primary">
           <SendIcon />
         </Button>
 
-        {/* Voice message */}
         <IconButton color="primary">
           <MicIcon />
         </IconButton>
--- src/components/left.tsx
+++ src/components/left.tsx
@@ -1,24 +1,66 @@
 import { Box, Button, List, ListItem, ListItemIcon, ListItemText, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from "@mui/icons-material/Search"
 import Person from "@mui/icons-material/Person"
+import { Folder, Group } from "@mui/icons-material"
+import AssistantPhotoIcon from '@mui/icons-material/AssistantPhoto';
+import ContactsIcon from '@mui/icons-material/Contacts';
+
 function Left() {
 
-
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+  const leftItems = [
+    {
+      label: "Contacts",
+      icon: <ContactsIcon fontSize='small'></ContactsIcon>
+    },
+    {
+      label: "Groupes",
+      icon: <Group fontSize='small'></Group>
+    },
+    {
+      label: "Channels",
+      icon: <AssistantPhotoIcon fontSize='small'></AssistantPhotoIcon>
+    },
+  ]
   return (
     <div className='w-full'>
-      <div className="flex">
-        <div className="menu shadow-2xl min-h-screen w-1/6 p-6 box-border">
-          <MenuIcon></MenuIcon>
+      <div className="flex relative">
+
+        <div className="menu shadow-2xl min-h-screen w-[85px] box-border flex flex-col gap-6 pt-4">
+
+          <button className='cursor-pointer' onClick={() => setIsOpenMenu(!isOpenMenu)}><MenuIcon></MenuIcon></button>
+          <div className='flex flex-col gap-3'>
+            {
+              leftItems.map((el, index) => (
+                <div key={"folder_" + index} className='flex flex-col pl-1 py-1 rounded-2xl shadow-[1px_1px_10px_rgba(1,1,1,0.8)] cursor-pointer'>
+                  <div className='rounded-full size-7 flex items-center justify-center'>
+                    {el.icon}
+                  </div>
+                  <small>{el.label}</small>
+                </div>
+              ))
+            }
+          </div>
         </div>
-        <div className="users w-full py-2 space-y-2.5 box-border">
+        <div onClick={() => setIsOpenMenu(!isOpenMenu)} className={`path absolute w-[400px] min-h-screen border-r-2 shadow-2xl transition-all bg-white z-20 ${isOpenMenu ? "left-0" : "-left-[402px]"}`}>
+            <div className='flex gap-4 p-6 inset-shadow-gray-200 shadow-2xl cursor-pointer items-end'>
+              <div className='rounded-full shadow-[1px_1px_5px_rgba(1,1,1,0.8)] size-9 flex justify-center items-center'>
+                <Person fontSize='large'></Person>
+              </div>
+              <h1 className='text-transparent bg-gradient-to-l from-[rgba(139,19,187,0.8)] to-[rgba(141,12,12,0.8)] bg-clip-text hover:shadow-[0px_5px_2px_rgba(1,1,1,0.8)]'>Fayzillo Ummatov</h1>
+            </div>
+        </div>
+
+        <div className="users w-full py-2 space-y-2.5 box-border relative z-0">
+
           <div className="head flex justify-between w-full shadow-2xl py-2.5">
             <TextField fullWidth size='small'>
 
             </TextField>
-            <Button>
-              <SearchIcon></SearchIcon>
+            <Button variant='contained' >
+              <SearchIcon ></SearchIcon>
             </Button>
           </div>
           <div className='w-full box-border'>
@@ -44,8 +86,11 @@ function Left() {
               </ListItem>
             </List>
           </div>
+
         </div>
+
       </div>
+
     </div>
   )
 }
